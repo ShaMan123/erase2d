aa020824508d36c0f2f72be0038476f4b100fe87
ShaMan123 <shacharnen@gmail.com>
2024-03-17 03:10:05 +0200

    sandbox app

---
+++ sandbox/README.md
@@ -0,0 +1,5 @@
+## Reproducing
+
+Provide a **detailed description** including steps to reproduce in the [`REPRODUCE.md`](./REPRODUCE.md) file.
+
+Navigate to [`src/index`](./src/index.ts) and start editing.
---
+++ sandbox/src/index.ts
@@ -0,0 +1,48 @@
+import { EraserBrush } from '@fabric/eraser2d';
+import * as fabric from 'fabric';
+import './styles.css';
+
+const el = document.getElementById('canvas');
+const canvas = (window.canvas = new fabric.Canvas(el));
+
+canvas.setDimensions({
+  width: window.innerWidth,
+  height: window.innerHeight,
+});
+
+const eraser = (canvas.freeDrawingBrush = new EraserBrush(canvas));
+eraser.width = 30;
+canvas.isDrawingMode = true;
+
+let state = 0;
+const states = [
+  { name: 'erasing', active: true, inverted: false },
+  { name: 'undoing erasing', active: true, inverted: true },
+  { name: 'default', active: true, inverted: false },
+];
+const button = new fabric.FabricText('', { backgroundColor: 'magenta' });
+const setState = (_state: number) => {
+  state = _state;
+  const { name, active, inverted } = states[state];
+  button.set('text', `Tool: ${name}`);
+  canvas.isDrawingMode = active;
+  eraser.inverted = inverted;
+  canvas.requestRenderAll();
+};
+setState(0);
+button.on('mouseup', ({ isClick }) => {
+  isClick && setState((state + 1) % 3);
+});
+
+canvas.add(
+  new fabric.Rect({ width: 500, height: 200, fill: 'blue', erasable: true }),
+  new fabric.Circle({ radius: 50, erasable: true }),
+  button
+);
+
+canvas
+  .item(0)
+  .animate(
+    { left: 500 },
+    { duration: 6000, onChange: () => canvas.renderAll() }
+  );
